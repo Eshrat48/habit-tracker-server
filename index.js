0ae7d348712e1eda8d557af1bf20b2700324b31a
Eshrat48 <eshratkamalnova@gmail.com>
2025-11-13 21:50:42 +0600

    Netlify Link Added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const getUserController = require('./controllers/userController');
 const userRoutes = require('./routes/userRoutes');
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:3000', 'https://habit-tracker24.netlify.app'],
     credentials: true,
 }));
 app.use(express.json());
